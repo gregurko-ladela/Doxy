57e2cfb3e340b75d1772082842000e1688f7026d
gregurko-ladela <gregurko@ladela.com>
2014-09-18 13:42:10 +0300

    fix

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 			<!-- Sidebar user panel -->
 			<div class="user-panel">
 				<div class="pull-left image">
-					<img src="img/avatar3.png" class="img-circle" alt="User Image" />
+					<img src="<?php echo get_template_directory_uri();?>/library/images/avatar3.png" class="img-circle" alt="User Image" />
 				</div>
 				<div class="pull-left info">
 					<p>Hello, Jane</p>
