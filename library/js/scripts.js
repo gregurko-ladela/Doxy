82313faaccba8690fc968443dc975e50eacfefdf
gregurko-ladela <gregurko@ladela.com>
2014-09-18 12:03:14 +0300

    Implement toggle of ledt menu and toggle of menu elements

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -106,3 +106,68 @@ jQuery(document).ready(function($) {
 	w.addEventListener( "orientationchange", restoreZoom, false );
 	w.addEventListener( "devicemotion", checkTilt, false );
 })( this );
+
+jQuery(function() {
+    "use strict";
+
+    //Enable sidebar toggle
+    jQuery("[data-toggle='offcanvas']").click(function(e) {
+        console.log('a');
+        e.preventDefault();
+
+        //If window is small enough, enable sidebar push menu
+        if (jQuery(window).width() <= 992) {
+            jQuery('.row-offcanvas').toggleClass('active');
+            jQuery('.left-side').removeClass("collapse-left");
+            jQuery(".right-side").removeClass("strech");
+            jQuery('.row-offcanvas').toggleClass("relative");
+        } else {
+            //Else, enable content streching
+            jQuery('.left-side').toggleClass("collapse-left");
+            jQuery(".right-side").toggleClass("strech");
+        }
+    });
+
+    jQuery.fn.tree = function() {
+
+        return this.each(function() {
+            var btn = jQuery(this).children("a").first();
+            var menu = jQuery(this).children(".treeview-menu").first();
+            var isActive = jQuery(this).hasClass('active');
+
+            //initialize already active menus
+            if (isActive) {
+                menu.show();
+                btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
+            }
+            //Slide open or close the menu on link click
+            btn.click(function(e) {
+                e.preventDefault();
+                if (isActive) {
+                    //Slide up to close menu
+                    menu.slideUp();
+                    isActive = false;
+                    btn.children(".fa-angle-down").first().removeClass("fa-angle-down").addClass("fa-angle-left");
+                    btn.parent("li").removeClass("active");
+                } else {
+                    //Slide down to open menu
+                    menu.slideDown();
+                    isActive = true;
+                    btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
+                    btn.parent("li").addClass("active");
+                }
+            });
+
+            /* Add margins to submenu elements to give it a tree look */
+            menu.find("li > a").each(function() {
+                var pad = parseInt(jQuery(this).css("margin-left")) + 10;
+
+                jQuery(this).css({"margin-left": pad + "px"});
+            });
+
+        });
+
+    };
+
+    jQuery(".sidebar .treeview").tree();
+});
